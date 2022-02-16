ffef0f2f886dba07664dd716ed67174c557acdd5
Keith Caulkins <keith.caulkins@platform.sh>
2022-02-15 19:11:06 -0500

    Add more tasks; Fix JS; switch to darkmode

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // 1. Save localstorage on checkbox click/unclick
 let boxes = document.getElementsByClassName('uk-checkbox').length;
-
 function save() {	
   for(let i = 1; i <= boxes; i++){
 	  var checkbox = document.getElementById(String(i));
@@ -8,13 +7,13 @@ function save() {
   }
 }
 
-function reset() {
-    document.querySelectorAll('input[type=checkbox]').forEach(el => el.checked = false);
-    for(let i = 1; i <= boxes; i++){
-        var checkbox = document.getElementById(String(i));
-      localStorage.setItem("checkbox" + String(i), checkbox.checked);	
-    }
-    console.info('reset');
+function reset(e) {
+    resetBoxes = e.parentElement.parentElement.querySelectorAll('input[type=checkbox]');
+    console.log(resetBoxes.length);
+    resetBoxes.forEach(element => {
+        element.checked = false;
+        localStorage.setItem("checkbox" + String(element.id), element.checked);
+    });
 }
 
 // On page load, mark the selected ones
